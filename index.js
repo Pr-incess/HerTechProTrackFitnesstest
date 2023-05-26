1cdfc1e5259e7639820f393dab6c0d420f520699
Pr-incess <ngwuprincess8@gmail.com>
2023-05-26 21:41:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+// const change = document.getElementById("First"); 
+
+const change = document.querySelector(".First"); 
+
+
+
+// const change = document.getElementById("First");
+
+change.addEventListener('click', function (event) {
+
+   event.target.style.background = "orange";
+   event.target.style.color = "white";
+});
+
+
+const change2 = document.querySelector(".Second");
+
+change2.addEventListener('click', function (event) {
+
+   event.target.style.background = "orange";
+   event.target.style.color = "white";
+});
+
+
+const change3 = document.querySelector(".Third");
+
+change3.addEventListener('click', function (event) {
+
+   event.target.style.background = "orange";
+   event.target.style.color = "white";
+});  
+
+
+const change4 = document.querySelector(".Four");
+
+change4.addEventListener('click', function (event) {
+
+   event.target.style.background = "orange";
+   event.target.style.color = "white";
+});
+
+
+const change5 = document.querySelector(".Five");
+
+change5.addEventListener('click', function (event) {
+
+   event.target.style.background = "orange";
+   event.target.style.color = "white";
+});
+
+const change6 = document.querySelector(".Six");
+
+change6.addEventListener('click', function (event) {
+
+   event.target.style.background = "orange";
+   event.target.style.color = "white";
+});
+
+const change7 = document.querySelector(".Seven");
+
+change7.addEventListener('click', function (event) {
+
+   event.target.style.background = "orange";
+   event.target.style.color = "white";
+});
+//(event) =>{
+   // event.target.style.background= "blue";
+//});
+
